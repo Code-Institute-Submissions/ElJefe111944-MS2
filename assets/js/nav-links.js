b94e1c46b1d2f30063ca3b9a014d347d32d5e4aa
ElJefe111944 <jamesfarr1@sky.com>
2020-09-20 13:22:30 +0000

    A function has been created in nav-links.js that upon the user clicking on the menu btn add the class two. Once the menu btn has been clicked again by the user the class two is removed and the class one is added. Both the classes one and two have been styled using a css clip-path maker which animates a circle expanding across the screen. This in turn opens the links to the nav bar.

--- assets/js/nav-links.js
+++ assets/js/nav-links.js
@@ -1,1 +1,15 @@
-alert();
+let btn = document.getElementById('menu');
+let navLinks = document.getElementById('links');
+let counter = 0;
+
+btn.addEventListener("click", function() {
+    if (counter == 0) {
+        navLinks.className = ("links two")
+        counter = 1;
+    } else {
+        navLinks.classList.remove("two")
+        navLinks.className = ("links one")
+        counter = 0;
+    }
+})
+
