7ee17711e263066ae3a0c7d38a7a39aa7992d155
ElJefe111944 <jamesfarr1@sky.com>
2020-09-26 14:39:17 +0000

    Variables declared in modal.js. An event listener now add the class .appear to the class .modal-background changing the opacity from zero to one. The event is triggered by clicking on one of the three landing images. In the next commit an event listener will be added to close the modal.

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,1 +1,17 @@
+const landingImages = document.querySelectorAll(".landing-imgs img");
+const modal = document.querySelector(".modal-background");
+const modalFullImage = document.querySelector(".modalFullImage");
+const modalCaption = document.querySelector(".modalCaption");
 
+
+landingImages.forEach((landingImage) => {
+    landingImage.addEventListener("click", () => {
+        modal.classList.add("appear");
+        modalFullImage.classList.add("appear");
+
+// Text and Image 
+const modalFullImageSrc = landingImage.getAttribute("data-original");
+modalFullImage.src = modalFullImageSrc;
+modalCaption.innerHTML = landingImage.alt;
+    })
+})
