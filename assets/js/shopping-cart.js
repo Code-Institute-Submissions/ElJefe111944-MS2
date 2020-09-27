e561ba256e280f428153ba4f198d67125608c370
ElJefe111944 <jamesfarr1@sky.com>
2020-09-27 13:00:07 +0000

    The class .shoppingCart has been added to the index.html page so the shopping cart can be accessed on either page by the user. The browser support for local storage has been tested using the test on W3Shool.com. Test passed so browser supports local storage.

--- assets/js/shopping-cart.js
+++ assets/js/shopping-cart.js
@@ -1,3 +1,21 @@
 window.onload = function(){
-  alert('Esta funcionando bien!');  
+   // Shopping Cart
+   const shoppingCartIcon = document.querySelector('.shoppingCartIcon');
+   const shoppingCart = document.querySelector('.shoppingCart');
+   const cartCloseBtn = document.querySelector('i.fa-times-circle');
+
+   shoppingCartIcon.addEventListener("click", function (){
+      shoppingCart.classList.add('open');
+  });
+   cartCloseBtn.addEventListener("click", function (){
+      shoppingCart.classList.remove('open');
+   })
+};
+
+// Local Storage to retain items selected by user.
+if(typeof(Storage) !== 'undefined'){
+    console.log('funciona');
+}else{
+    console.log('no funciona');
 }
+
