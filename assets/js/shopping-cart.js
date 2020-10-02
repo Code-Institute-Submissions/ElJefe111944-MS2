470dea3c466df022204e3a9779269dab6e5807b0
ElJefe111944 <jamesfarr1@sky.com>
2020-10-02 11:11:39 +0000

    An event listener has been included in the shopping-cart.js file to add the class .open to the .shoppingCartIcon class when the user clicks on the shopping cart icon. A for loop has also been created to select the variables in the object item  when the user clicks on the addToShoppingCartBtn and to push these onject into an array and store them in the Local Storage. Finally for each item stored in the local storage the shoppingCartIcon p is updated with the number of items selected by the user. I.e the number of items in the array.

--- assets/js/shopping-cart.js
+++ assets/js/shopping-cart.js
@@ -12,10 +12,57 @@ window.onload = function(){
    })
 };
 
-// Local Storage to retain items selected by user.
-if(typeof(Storage) !== 'undefined'){
-    console.log('funciona');
-}else{
-    console.log('no funciona');
-}
+// Adding product selected by user to local storage.
+const addToShoppingCartBtn = document.getElementsByClassName('addToShoppingCartBtn')
+let items = [];
+
+for (let i=0; i < addToShoppingCartBtn.length; i++){
+    addToShoppingCartBtn[i].addEventListener("click", function(e){
+        if(typeof(Storage) !== 'undefined'){
+           let item = {
+           
+           
+              id:i+1,
+              name: e.target.parentElement.children[0].textContent,
+              price:e.target.parentElement.children[1].children[0].textContent,
+              no:1
+            
+           }; 
+        
+
+            if (JSON.parse(localStorage.getItem('items')) === null){
+                items.push(item);  //Pushing items into empty array
+                localStorage.setItem("items", JSON.stringify(items));
+                window.location.reload();
+            } else {
+const localItems = JSON.parse(localStorage.getItem("items"));
+					localItems.map(data=>{
+						if(item.id == data.id){
+							item.no = data.no + 1;
+						}else{
+							items.push(data);
+						}
+					});
+					items.push(item);
+					localStorage.setItem('items',JSON.stringify(items));
+					window.location.reload();
+				}
+			}else{
+				alert('Sorry! No Web Storage support..');
+			}
+		});
+	}
+// shoppingCartIcon updated with number of items selected.
+const shoppingCartIcon = document.querySelector('.shoppingCartIcon p');
+let no = 0;
+JSON.parse(localStorage.getItem('items')).map(data=> {
+     no = no+data.no
+     shoppingCartIcon.textContent = no;
+
+});
+
+
+
+
+
 
