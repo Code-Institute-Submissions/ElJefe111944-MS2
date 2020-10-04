8a66ce3c2090e566fbec89d8e1c1358d751556ee
ElJefe111944 <jamesfarr1@sky.com>
2020-10-04 12:32:54 +0000

    tesitmonials.html changed to reviews.html. testimonials.js changed to reviews.js. reviews.js contains the javascript for the image slider. The classes and css has been changed froms testimonials to reviews.

---
+++ assets/js/reviews.js
@@ -0,0 +1,27 @@
+let slider = document.querySelector('.slider-container');
+let eachSlide = document.querySelectorAll('.reviews');
+let count = 0;
+let sizeWidth = eachSlide[0].clientWidth;
+let interval = 3000;
+
+window.addEventListener("resize", function(){
+    sizeWidth = eachSlide[0].clientWidth;
+})
+
+setInterval(function time (){
+    slides();
+}, interval)
+
+function slides(){
+    slider.style.transform = 'translate(' + (- sizeWidth * count) + 'px';
+    slider.style.transition = 'tranform 5s';
+    count++;
+
+    if(count === eachSlide.length){
+        count = 0;
+    setInterval(function(){
+      slider.style.transform = 'translate(' + (- sizeWidth * count) + 'px';
+      slider.style.transition = 'tranform 5s';  
+    }, interval)
+    }
+}
