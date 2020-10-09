e9dbce5759130741dc73085d7d560f51cc58a1c8
ElJefe111944 <jamesfarr1@sky.com>
2020-10-09 22:11:18 +0000

    The Javascript for the input validator has been created in contact-us.js. An event listener has been created so that when the user clicks on the submit button the validator is run to verify that the name, email and message are of the correct format. I.e are over a certain length and the email contains certain characters. If any of the three are not valid an error message is sent to the paragraph tags with the class warnings informing the user that one of the inputs is invalid.

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -3,3 +3,30 @@ const email = document.getElementById('email');
 const message = document.getElementById('message');
 const form = document.getElementById('form');
 const errorMessage = document.getElementById('warnings');
+
+form.addEventListener("submit", e=>{
+    e.preventDefault()
+    let warnings = ""
+    let entrar = false
+    var emailValidator = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/; 
+    errorMessage.innerHTML = ""
+    
+    if (name.value.length < 3){
+        warnings += 'Invalid Name <br>'
+          entrar = true
+    }   
+     
+    if (!emailValidator.test(email.value)){
+        warnings += "Invalid Email <br>"
+        entrar = true
+    }
+    if(message.value.length <= 10){
+        warnings += "Please Enter More Than 10 Characters<br>"
+        entrar = true
+    }
+    if (entrar){
+        errorMessage.innerHTML = warnings;
+    } else {
+        errorMessage.innerHTML = 'Sent'
+    }
+})
