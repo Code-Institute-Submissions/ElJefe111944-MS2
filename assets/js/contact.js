a45f745452063fdcbf6fdd7113d95fc455053c27
ElJefe111944 <jamesfarr1@sky.com>
2020-10-13 22:26:45 +0000

    The constants messageSent, errorName, errorEmail and error message have been added to contact.js. These classes hace also been added to the p tag underneath each corresponding input field. A error message will appear if the user put in an invalid input for each field. Also the class sent has been added so the user is informed that the message has been sent succesfully.

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -2,31 +2,40 @@ const name = document.getElementById('name');
 const email = document.getElementById('email');
 const message = document.getElementById('message');
 const form = document.getElementById('form');
-const errorMessage = document.getElementById('warnings');
+const errorMessage = document.getElementById('messageError');
+const messageSent = document.getElementById('sent');
+const errorName = document.getElementById('nameError');
+const errorEmail = document.getElementById('emailError');
 
 form.addEventListener("submit", e=>{
     e.preventDefault()
     let warnings = ""
+    let nameError = ""
+    let emailError = ""
+    let messageError = ""
     let enter = false
     var emailValidator = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/; 
     errorMessage.innerHTML = ""
     
     if (name.value.length < 3){
-        warnings += 'Invalid Name <br>'
+        nameError += 'Invalid Name <br>'
           enter = true
     }   
      
     if (!emailValidator.test(email.value)){
-        warnings += "Invalid Email <br>"
+        emailError += "Invalid Email <br>"
         enter = true
     }
     if(message.value.length <= 10){
-        warnings += "Please Enter More Than 10 Characters<br>"
+        messageError += "Please Enter More Than 10 Characters<br>"
         enter = true
     }
     if (enter){
-        errorMessage.innerHTML = warnings;
+        errorMessage.innerHTML = messageError;
+        errorName.innerHTML = nameError;
+        errorEmail.innerHTML = emailError;
+
     } else {
-        errorMessage.innerHTML = 'Your Message Has Been Sent'
+        messageSent.innerHTML = 'Your Message Has Been Sent'
     }
 })
